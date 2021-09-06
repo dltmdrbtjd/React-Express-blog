e451e5d5127f191cf2587e8168ab62e78dd93dc2
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-09-07 02:30:52 +0900

    mongodb atlas 연결 및 회원가입 api연결

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -1,6 +1,21 @@
-import mongoose from 'mongoose';
-import 'dotenv/config';
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './Loaders/db';
 
-const { PORT, MONGO_URI } = process.env;
+dotenv.config();
 
-mongoose.connect(`${MONGO_URI}`, { useNewUrlParser: true });
+connectDB();
+
+const app = express();
+
+// local 포트번호
+const { PORT } = process.env;
+
+// body-parser 
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+
+// routers
+app.use('/api/register', require('./routes/users'));
+
+app.listen(PORT, () => console.log(`Server on port ${PORT}`));
