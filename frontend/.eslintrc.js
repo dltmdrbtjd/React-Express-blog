692b714d290506638432ef34e4d334d896e9d15d
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-09-07 23:57:37 +0900

    typescript/eslint/prettier 환경설정 및 셋팅

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  root: true,
+  extends: [
+    'airbnb',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      tsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  plugins: ['react', '@typescript-eslint','prettier'],
+  rules: {
+    'prefer-destructuring':0,
+    'no-use-before-define':0,
+    '@typescript-eslint/explicit-module-boundary-types':0,
+    '@typescript-eslint/no-explicit-any':0,
+    'prettier/prettier':0,
+    'react/prop-types': 0,
+    'no-undef': 0,
+    'import/no-unresolved': 0,
+    'react/require-default-props': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/extensions':0,
+    'import/prefer-default-export':0,
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      },
+    ]
+  },
+};
