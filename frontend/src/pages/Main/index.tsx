77b891b630ad2f33cabd8b37f2a15687ebe4d27d
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-09-24 23:40:55 +0900

    redux-saga 연결 후 test작업 완료

--- frontend/src/pages/Main/index.tsx
+++ frontend/src/pages/Main/index.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { loadPostRequest } from 'src/modules/actions/post';
+import { useTypedSelector } from 'src/modules/store';
 
 const MainPage = () => {
-  return <div>메인페이지에용</div>;
+  const dispatch = useDispatch();
+  const list = useTypedSelector((state) => state.post.list);
+
+  useEffect(() => {
+    dispatch(loadPostRequest());
+  }, []);
+
+  return (
+    <div>
+      <h1>Redux-saga Test</h1>
+      {list.map((item: any) => {
+        return (
+          <div key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.body}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default MainPage;
