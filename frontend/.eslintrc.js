0c1043edfefb6f3701fe6e79bf805dc13c015be3
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-09-24 23:38:29 +0900

    eslint 설정 변경 및 redux-saga,immer추가

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -27,25 +27,27 @@ module.exports = {
       },
     },
   },
-  plugins: ['react', '@typescript-eslint','prettier'],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
-    'prefer-destructuring':0,
-    'no-use-before-define':0,
-    '@typescript-eslint/explicit-module-boundary-types':0,
-    '@typescript-eslint/no-explicit-any':0,
-    'prettier/prettier':0,
+    'consistent-return': 0,
+    'no-param-reassign': 0,
+    'prefer-destructuring': 0,
+    'no-use-before-define': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    'prettier/prettier': 0,
     'react/prop-types': 0,
     'no-undef': 0,
     'import/no-unresolved': 0,
     'react/require-default-props': 0,
     'import/no-extraneous-dependencies': 0,
-    'import/extensions':0,
-    'import/prefer-default-export':0,
+    'import/extensions': 0,
+    'import/prefer-default-export': 0,
     'react/jsx-filename-extension': [
       1,
       {
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
-    ]
+    ],
   },
 };
