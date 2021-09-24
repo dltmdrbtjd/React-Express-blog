44c43ccb02b1dcd503bfef837a32e43e260a2bd6
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-09-24 23:38:59 +0900

    redux-saga test용 더미 data 받아올 api

--- frontend/src/libs/api.ts
+++ frontend/src/libs/api.ts
@@ -1,3 +1,8 @@
-import axios from './axios';
+import axios from 'axios';
+import instance from './axios';
 
-export const UserLogin = (userinfo) => axios.post('/api/login', userinfo);
+export const UserLogin = (userinfo) => instance.post('/api/login', userinfo);
+
+export const loadPostAPI = () => {
+  return axios.get('https://jsonplaceholder.typicode.com/posts');
+};
