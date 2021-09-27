7a081585d75ccff6f3fb372cf32573fd9cb90242
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-09-28 00:42:30 +0900

    Feat: atom/Img src check

---
+++ frontend/src/components/atom/Img/index.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Img from './index';
+
+export default {
+  title: 'Atoms / Img',
+  component: Img,
+};
+
+export const Cat = () => (
+  <Img src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg" />
+);
