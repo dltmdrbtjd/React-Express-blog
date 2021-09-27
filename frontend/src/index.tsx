dd3556c6cfef58d8d0c089881300e4156d29b9d7
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-09-27 23:31:43 +0900

    connectedRouter-history 연결

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './modules/store';
+import { history } from './modules/reducers';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
