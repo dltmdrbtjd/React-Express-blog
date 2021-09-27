f7b805610273af086bbdcc38cb1d8fa78fdd3849
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-09-27 23:31:30 +0900

    login 컴포넌트 주석처리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 // router
-import LoginPage from './pages/Login';
+// import LoginPage from './pages/Login';
 import MainPage from './pages/Main';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={MainPage} exact />
-        <Route path="/login" component={LoginPage} exact />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route path="/" component={MainPage} exact />
+      {/* <Route path="/login" component={LoginPage} exact /> */}
+    </Switch>
   );
 }
 
