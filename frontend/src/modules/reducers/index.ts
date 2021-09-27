47e0bcc0beac8f05e153c4aebb025ca4bb689aea
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-09-27 23:33:18 +0900

    rootReducer router(history)추가

--- frontend/src/modules/reducers/index.ts
+++ frontend/src/modules/reducers/index.ts
@@ -1,7 +1,12 @@
 import { combineReducers } from 'redux';
 
+import { createBrowserHistory } from 'history';
+import { connectRouter } from 'connected-react-router';
 import { postRedcuer } from './post';
+
+export const history = createBrowserHistory();
 
 export default combineReducers({
   post: postRedcuer,
-})
+  router: connectRouter(history),
+});
