381613c36cfd3ab67aa16bcd1e61d9cc828a56f3
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-09-27 23:31:57 +0900

    atom/Grid div tag 추가

---
+++ frontend/src/components/atom/Grid/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import * as S from './style';
+
+type Props = {
+  children: any;
+  className?: string;
+  onClick?: any;
+};
+
+function Grid({ children, className, onClick }: Props) {
+  return (
+    <S.Grid className={className} onClick={onClick}>
+      {children}
+    </S.Grid>
+  );
+}
+
+export default Grid;
