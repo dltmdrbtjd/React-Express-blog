e1d562fc26cd2da6d882e6c423473244235dbadd
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-09-27 23:35:41 +0900

    reset css 및 기본 css 설정

---
+++ frontend/src/styles/GlobalStyle.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+  body {
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+    box-sizing: border-box;
+    line-height: 1.5;
+    font-family: 'NanumSquare', sans-serif;
+  }
+
+  * {
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+    margin: 0;
+    padding: 0;
+    font-family: 'NanumSquare', sans-serif;
+  }
+
+  button, input {
+    -moz-appearance: none; /* Firefox */
+    -webkit-appearance: none; /* Safari and Chrome */
+    appearance: none;
+  }
+  menu, ol, ul {
+    list-style: none;
+  }
+`;
+
+export default GlobalStyle;
