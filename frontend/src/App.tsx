5612d69afa8c78fa0509ca7388e2c0358889021c
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-10-01 18:40:37 +0900

    Feat: Posting Page add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import PostCreate from './components/organisms/PostCreate';
 // router
 // import LoginPage from './pages/Login';
 import MainPage from './pages/Main';
@@ -8,6 +9,7 @@ function App() {
   return (
     <Switch>
       <Route path="/" component={MainPage} exact />
+      <Route path="/posting" component={PostCreate} exact />
       {/* <Route path="/login" component={LoginPage} exact /> */}
     </Switch>
   );
