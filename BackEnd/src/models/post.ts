87b2f0f03612200df83e514abea54974a9536b69
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-10-03 20:26:17 +0900

    Feat: Schema tags,category Add

--- BackEnd/src/models/post.ts
+++ BackEnd/src/models/post.ts
@@ -4,11 +4,15 @@ type IPost = {
   _id: string;
   title: string;
   content: string;
+  category: string;
+  tags: string[];
 };
 
 export const postSchema = new mongoose.Schema({
   title: { type: String },
   content: { type: String },
+  category: { type: String },
+  tags: { type: Array },
 });
 
 export default mongoose.model<IPost & mongoose.Document>('Post', postSchema);
