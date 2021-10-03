a4ac1b976174ded8d5e853ddba1b833fab8188d0
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-10-03 20:26:29 +0900

    Feat: connect post router

--- BackEnd/src/routes/index.ts
+++ BackEnd/src/routes/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import registerRouter from './register';
 import loginRouter from './login';
+import postRouter from './post';
 import auth from '../middleware/auth';
 
 const router = express.Router();
 
 router.use('/register', registerRouter);
 router.use('/login', loginRouter);
+router.use('/post', postRouter);
 router.use(auth);
 
 export default router;
