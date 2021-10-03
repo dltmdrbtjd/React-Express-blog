ec8da535d3e962b6636bbe4d877178a020c00ff9
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-10-03 20:27:32 +0900

    Feat: req,res type add

--- BackEnd/src/routes/register.ts
+++ BackEnd/src/routes/register.ts
@@ -1,16 +1,18 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import User from '../models/user';
 
 const router = express.Router();
 
-router.post('/', async (req,res) => {
+router.post('/', async (req: Request, res: Response) => {
   const { name, userid, password } = req.body;
 
   try {
     let user = await User.findOne({ userid, name });
-    if(user){
-      return res.status(400).json({ error: [{ msg: 'userid or name already exists!'}]});
+    if (user) {
+      return res
+        .status(400)
+        .json({ error: [{ msg: 'userid or name already exists!' }] });
     }
 
     user = new User({
@@ -23,8 +25,8 @@ router.post('/', async (req,res) => {
     user.password = await bcrypt.hash(password, salt);
 
     await user.save();
-    return res.status(201).send({ message: '회원가입이 완료되었습니다.'})
-  } catch(err) {
+    return res.status(201).send({ message: '회원가입이 완료되었습니다.' });
+  } catch (err) {
     console.error(err);
     res.status(500).send('Server Error');
   }
