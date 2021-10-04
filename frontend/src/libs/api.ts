05127e58762c858626b3bb82f38ccbe840514927
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-10-04 21:37:13 +0900

    Feat: Add CreatePostApi

--- frontend/src/libs/api.ts
+++ frontend/src/libs/api.ts
@@ -3,6 +3,6 @@ import instance from './axios';
 
 export const UserLogin = (userinfo) => instance.post('/api/login', userinfo);
 
-export const loadPostAPI = () => {
-  return axios.get('https://jsonplaceholder.typicode.com/posts');
-};
+export const loadPostAPI = () => instance.get('/api/post');
+
+export const CreatePostApi = (list) => instance.post('/api/post', list);
