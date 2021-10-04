b3b8bdf235ddfa09ed1a98e2d9f755f791750734
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-10-04 21:36:19 +0900

    Feat: fetch/dispatch postlist

--- frontend/src/components/templates/Main/index.tsx
+++ frontend/src/components/templates/Main/index.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Category from 'src/components/organisms/Category';
 import Header from 'src/components/organisms/Header';
 import PostList from 'src/components/organisms/PostList';
+import { loadPostRequest } from 'src/modules/actions/post';
+import { useTypedSelector } from 'src/modules/store';
 import * as S from './style';
 
 function MainTemplate() {
+  const dispatch = useDispatch();
+  const posts = useTypedSelector((state) => state.post.list);
+
+  useEffect(() => {
+    dispatch(loadPostRequest());
+  }, []);
+
   return (
     <>
       <Header />
       <S.MainWrapper>
         <S.MainSection>
           <Category />
-          <PostList />
+          <S.PostSection>
+            {posts.map((item: any) => {
+              return <PostList key={item._id} list={item} />;
+            })}
+          </S.PostSection>
         </S.MainSection>
       </S.MainWrapper>
     </>
