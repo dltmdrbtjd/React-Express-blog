366f76645b645852192d7758474f97a1e20cb4ec
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-10-05 21:03:06 +0900

    Feat: Add Move to detail page function

--- frontend/src/components/organisms/PostList/index.tsx
+++ frontend/src/components/organisms/PostList/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { history } from 'src/modules/reducers';
 import * as S from './style';
 
 function PostList({ list }) {
   const date = list.time.split('T');
+
+  function MoveToDetail() {
+    history.push(`/post/${list._id}`);
+  }
   return (
-    <S.PostListWrapper>
+    <S.PostListWrapper onClick={() => MoveToDetail()}>
       <S.PostListTitle>{list.title}</S.PostListTitle>
       <S.PostListSection>
         <S.PostListText>{list.category}</S.PostListText>
