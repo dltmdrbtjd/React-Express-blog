9d7b6187d3cbe7f5429dbe55045e1b6a1692affd
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-10-05 21:01:57 +0900

    Feat: Add post detail page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import PostCreate from './components/organisms/PostCreate';
+import Posting from './pages/Posting';
 // router
 // import LoginPage from './pages/Login';
 import MainPage from './pages/Main';
+import Post from './pages/Post';
 
 function App() {
   return (
     <Switch>
       <Route path="/" component={MainPage} exact />
-      <Route path="/posting" component={PostCreate} exact />
+      <Route path="/posting" component={Posting} exact />
+      <Route path="/post/:postId" component={Post} exact />
       {/* <Route path="/login" component={LoginPage} exact /> */}
     </Switch>
   );
