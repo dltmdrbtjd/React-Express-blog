48e5f6f27b4d588cfed8975df03f401cf0d220a9
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-10-05 21:02:42 +0900

    Feat: Add PostDetail style

---
+++ frontend/src/components/organisms/Post/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import Grid from 'src/components/atom/Grid';
+
+export const MainWrapper = styled(Grid)`
+  max-width: 768px;
+  width: 100%;
+  margin: 0 auto;
+  overflow: hidden;
+  margin-bottom: 30px;
+`;
