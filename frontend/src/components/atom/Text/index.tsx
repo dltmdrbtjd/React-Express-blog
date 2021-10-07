fb7740fd0aae3e8a86a60ffd6524445124801eec
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-10-07 20:50:00 +0900

    Feat: Add onClick type

--- frontend/src/components/atom/Text/index.tsx
+++ frontend/src/components/atom/Text/index.tsx
@@ -4,10 +4,15 @@ import * as S from './style';
 type Props = {
   className?: string;
   children: string;
+  onClick?: () => void;
 };
 
-function Text({ children, className }: Props) {
-  return <S.Text className={className}>{children}</S.Text>;
+function Text({ children, className, onClick }: Props) {
+  return (
+    <S.Text className={className} onClick={onClick}>
+      {children}
+    </S.Text>
+  );
 }
 
 export default Text;
