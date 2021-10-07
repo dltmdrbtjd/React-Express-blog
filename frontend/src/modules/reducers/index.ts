c0e9fe197efffc9fed30bebe0fc9855fe78d8ea5
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-10-07 20:53:07 +0900

    Feat: Add detailReducer

--- frontend/src/modules/reducers/index.ts
+++ frontend/src/modules/reducers/index.ts
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux';
 import { createBrowserHistory } from 'history';
 import { connectRouter } from 'connected-react-router';
 import { postRedcuer } from './post';
+import { detailReducer } from './detail';
 
 export const history = createBrowserHistory();
 
 export default combineReducers({
   post: postRedcuer,
+  detail: detailReducer,
   router: connectRouter(history),
 });
